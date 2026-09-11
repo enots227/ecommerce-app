0211489f8c5597ec1dd75cb86a7836c2512b9d6b
Stone Sommers <enots227@gmail.com>
2026-09-11 12:54:18 -0700

    change theme color

--- ui/app/layout.tsx
+++ ui/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <Theme>{children}</Theme>
+        <Theme accentColor="tomato">{children}</Theme>
       </body>
     </html>
   );
